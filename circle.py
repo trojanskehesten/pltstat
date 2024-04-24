86cb64faa8140de75c7a3897e2c670d1a742add0
trojanskehesten <forth08@yandex.ru>
2024-04-24 01:19:40 +0100

    feat/090301: Add docstrings to circle.py, correct bugs

--- circle.py
+++ circle.py
@@ -2,49 +2,16 @@
 import matplotlib.pyplot as plt
 from matplotlib import ticker
 import numpy as np
-
-_HIGH=24
-
-
-"""Return a list of the marginal sums of the array `a`.
-
-    Parameters
-    ----------
-    a : ndarray
-        The array for which to compute the marginal sums.
-
-    Returns
-    -------
-    margsums : list of ndarrays
-        A list of length `a.ndim`.  `margsums[k]` is the result
-        of summing `a` over all axes except `k`; it has the same
-        number of dimensions as `a`, but the length of each axis
-        except axis `k` will be 1.
-
-    Examples
-    --------
-    >>> import numpy as np
-    >>> from scipy.stats.contingency import margins
-
-    >>> a = np.arange(12).reshape(2, 6)
-    >>> a
-    array([[ 0,  1,  2,  3,  4,  5],
-           [ 6,  7,  8,  9, 10, 11]])
-    >>> m0, m1 = margins(a)
-    >>> m0
-    array([[15],
-           [51]])
-    >>> m1
-    array([[ 6,  8, 10, 12, 14, 16]])
-
-"""
+import seaborn as sns
 
 
-
+_HIGH = 24
+_N_BINS = 24
+_N_TICKS = 24
 
 
 def rad2val(a, high=_HIGH):
-    """Convert values `a` from radians to measures with the highest value `high`
+    """Convert values ``a`` from radians to measures with the highest value ``high``
 
     Parameters
     ----------
@@ -56,7 +23,7 @@ def rad2val(a, high=_HIGH):
     Returns
     -------
     rad2val : ndarray
-        The corresponding values with `high` boundary. This is a scalar if `a` is a scalar.
+        The corresponding values with `high` boundary. This is a scalar if ``a`` is a scalar.
 
     Example
     --------
@@ -71,20 +38,21 @@ def rad2val(a, high=_HIGH):
     a = a / np.pi / 2 * high
     return a
 
+
 def val2rad(a, high=_HIGH):
     """Convert values `a` from measures with the highest value `high` to radians
 
         Parameters
         ----------
         a : array_like
-            Input array in measures with the `high` boundary fot the sample range
+            Input array in measures with the ``high`` boundary fot the sample range
         high : float or int
             High boundary for the sample range
 
         Returns
         -------
         rad2val : ndarray
-            The corresponding radian values. This is a scalar if `a` is a scalar.
+            The corresponding radian values. This is a scalar if ``a`` is a scalar.
 
         Example
         --------
@@ -102,29 +70,19 @@ def val2rad(a, high=_HIGH):
     a = a / high * 2 * np.pi
     return a
 
-def hist(
-    a, n_bins, high=_HIGH, bottom=0.1, title=None, figsize=None, ax=None, return_ax=False, **kwargs,
-):
-    """
-    Histogram for time data
 
-    :param a: numpy array of hours
-    :param n_bins: count of bins
-    :param high: the highest possible value
-    :param bottom: proportion
-    :param title: title of histogram, is None - no title
-    :param figsize: tuple
-    :param ax: ax from matplotlib which is used for plottng. If None - function
-      will create new ax
-    :param return_ax: if True return ax, else return None
-    :return: ax if return_ax is True, else - return None
-    """
+def hist(
+    a, n_bins=_N_BINS, high=_HIGH, bottom=0.1, title=None, figsize=None, ax=None, return_ax=False, **kwargs,
+):
+    if n_bins < 2:
+        raise ValueError("Received an invalid number of bins. Number of bins must be at least 2, and must be an int.")
 
     a = a / high * 2 * np.pi  # h to rad
 
     def radian_function(x, y):
-        rad_x = x / np.pi
-        return f"{str(round(rad_x * 12, 1))} h"
+        """Helper function for labeling the x-axis"""
+        rad_x = x / np.pi / 2
+        return f"{(rad_x * high):.3g}"
 
     theta = np.linspace(0, 2 * np.pi, n_bins, endpoint=False)
     bins = np.linspace(0, 2 * np.pi, n_bins + 1, endpoint=True)
@@ -169,16 +127,50 @@ def hist(
     #     bar.set_alpha(0.8)
 
 
+hist.__doc__ = """\
+Plot histogram to show distributions of circular datasets.
+
+Parameters
+----------
+a : array_like
+    Input array in measures with the ``high`` boundary for the sample range.
+n_bins : int, default: {n_bins}
+    The number of bins to produce. Raises ValueError if ``n_bins < 2``.
+high : float or int, default: {high}
+    High boundary for the sample range.
+bottom : float, default: 0.1
+    Proportion of location of the bottom of each bin, where 0 is center of the circle, and 1 is the edge.
+    Bins are drawn from ``bottom * max(bins)`` to ``bottom * max(bins) + hist(x, bins)``.
+    Valid range is [0, 1].
+title : str or None, default: None
+    Text to use for the title.
+figsize : (float, float) or None, default: None
+    Width, height in inches.
+ax : :class:`matplotlib.axes.Axes` or array of Axes or None, default: None
+   Axes object to draw the plot onto, otherwise uses the current Axes. If ``ax`` is None, create nex ``ax``.
+return_ax : bool, default: False
+    Show, it is necessary to return ``ax``.
+kwargs : key, value mappings
+    Other keywords arguments are passed down to :meth:`maptplotlib.axes.Axes.hist`.
+
+Returns
+-------
+ax : :class:`matplotlib.axes.Axes` or array of Axes
+    Returns the Axes object with the plot drawn onto it if ``return_ax argument`` is ``True``.
+
+Example
+--------
+>>> import numpy as np
+>>> from pltstat.circle import hist
+>>> np.random.seed(0)
+>>> a = np.random.randint(0, 24, 30)
+>>> a
+array([ 6, 23, 11, 14, 18,  0, 14,  3, 21, 12, 10, 20, 11,  4,  6,  4, 15,
+       20,  3, 12,  4, 20,  8, 14, 15, 20,  3, 23, 15, 13])
+>>> hist(a, 12)""".format(n_bins=_N_BINS, high=_HIGH)
+
+
 def mean(a, high=_HIGH, atol=1e-10):
-    """
-    Get mean of numpy array with directional data
-    :param a: list of values,
-    :param high: the highest possible value,
-    :param atol: tolerance of radius-vector calculation.
-    If the radius is less than tolerance, it is impossible to calculate mean value.
-    1e-10 is enough for getting the mean value with 1e-07 accuracy
-    :return: circle mean value
-    """
     # to rad
     a = np.array(a) / high * 2 * np.pi
 
@@ -198,18 +190,46 @@ def mean(a, high=_HIGH, atol=1e-10):
     tau = tau / np.pi / 2 * high
     if tau < 0:
         tau += high
-    elif tau >= high:
+    # instead of elif, because-1.614809932057922e-15 + 360 => 360, for example a=[10, 350], high=360:
+    if tau >= high:
         tau -= high
     return tau
 
 
+mean.__doc__ = """\
+    Compute the circular mean for samples in a range. 
+    The function shows more accurate result than :meth:`scipy.stats.circmean`
+
+    Parameters
+    ----------
+    a : array_like
+        Input array in measures with the ``high`` boundary for the sample range.
+    high : float or int, default: {high}
+        High boundary for the sample range.
+    atol : float, default: 1e-10
+        The threshold for radius calculation. If the radius is less than ``atol``, it will be set to 0 and the mean value
+        will be NaN. When the radius is 0, it is not possible to calculate the mean value. If the radius is close to 0,
+        the mean value can be extremly inaccurate.
+        An ``atol`` equal to ``1e-10`` ensures a mean value accurate to approximately 7 decimal places. 
+
+    Returns
+    -------
+    mean : float
+        Circular mean
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from pltstat.circle import mean
+    >>> from scipy.stats import circmean
+    >>> a = [10, 350]
+    >>> mean(a, 360)
+    0.0
+    >>> circmean(a, 360)
+    359.99999999999994""".format(high=_HIGH)
+
+
 def std(a, high=_HIGH):
-    """
-    Get std of numpy array with directional data
-    :param a: list of values,
-    :param high: the highest possible value,
-    :return: circle std value
-    """
     # to rad
     a = np.array(a) / high * 2 * np.pi
 
@@ -228,3 +248,113 @@ def std(a, high=_HIGH):
     # return from rad
     v = v / np.pi / 2 * high
     return v
+
+
+std.__doc__ = """\
+    Compute the circular standard deviation for samples in a range.
+
+    Parameters
+    ----------
+    a : array_like
+        Input array in measures with the ``high`` boundary for the sample range.
+    high : float or int, default: {high}
+        High boundary for the sample range. 
+
+    Returns
+    -------
+    std : float
+        Circular standard deviation
+
+    Examples
+    --------
+    >>> import numpy as np
+    >>> from pltstat.circle import std
+    >>> from scipy.stats import circstd
+    >>> a = [10, 350]
+    >>> std(a, 360)
+    10.0255602484647
+    >>> circstd(a, 360)
+    10.025560248464737""".format(high=_HIGH)
+
+
+def scatter(
+    deg, y, high=_HIGH, n_ticks=_N_TICKS, title=None, figsize=None, ax=None, return_ax=False, **kwargs
+):
+    BOTTOM_EDGE = 0.2
+    UPPER_EDGE = 0.1
+
+    rads = deg / high * 2 * np.pi  # h to rad
+    theta = np.linspace(0, 2 * np.pi, n_ticks, endpoint=False)
+
+    def radian_function(x, y):
+        """Helper function for labeling the x-axis"""
+        rad_x = x / np.pi / 2
+        return f"{(rad_x * high):.3g}"
+
+    if ax is None:
+        fig, ax = plt.subplots(subplot_kw={"projection": "polar"}, figsize=figsize)
+
+    y_max = max(y)
+    y_min = min(y)
+    y_diff = y_max - y_min
+
+    sns.scatterplot(x=rads, y=y, ax=ax, **kwargs)
+    ax.set_ylim((y_min - y_diff * BOTTOM_EDGE, y_max + y_diff * UPPER_EDGE))
+
+    # arrange graph
+    ax.set(
+        theta_offset=np.pi / 2,
+        theta_direction=-1,
+        xticks=theta,
+        # yticks=yticks,
+        # yticklabels=yticks,
+    )
+    ax.xaxis.set_major_formatter(ticker.FuncFormatter(radian_function))
+    ax.set_title(title)
+    if return_ax:
+        return ax
+
+
+scatter.__doc__ = """\
+Draw a scatter plot of circular datasets.
+
+Parameters
+----------
+deg : array_like
+    Variables that specify positions on the angle axis.
+y : array_like
+    Variables that specify positions on the y axis.
+high : float or int, default: {high}
+    High boundary for the sample range.
+n_ticks : int, default: {n_ticks}
+    The number of angle ticks to produce.
+ax : :class:`matplotlib.axes.Axes` or array of Axes or None, default: None
+   Axes object to draw the plot onto, otherwise uses the current Axes. If ``ax`` is None, create nex ``ax``.
+return_ax : bool, default: False
+    Show, it is necessary to return ``ax``.
+kwargs : key, value mappings
+    Other keywords arguments are passed down to :meth:`seaborn.scatterplot`.
+
+Returns
+-------
+ax : :class:`matplotlib.axes.Axes` or array of Axes
+    Returns the Axes object with the plot drawn onto it if ``return_ax argument`` is ``True``.
+
+
+    :return: ax if return_ax is True, else - return None
+
+Example
+--------
+>>> import numpy as np
+>>> from pltstat.circle import scatter
+>>> np.random.seed(0)
+>>> deg = np.linspace(0, 24, 20, endpoint=False)
+>>> deg
+array([ 0. ,  1.2,  2.4,  3.6,  4.8,  6. ,  7.2,  8.4,  9.6, 10.8, 12. ,
+       13.2, 14.4, 15.6, 16.8, 18. , 19.2, 20.4, 21.6, 22.8])
+>>> temp = np.concatenate((np.repeat([36.6], 10), np.linspace(36.6, 40, 10, endpoint=False)))
+>>> temp
+array([36.6 , 36.6 , 36.6 , 36.6 , 36.6 , 36.6 , 36.6 , 36.6 , 36.6 ,
+       36.6 , 36.6 , 36.94, 37.28, 37.62, 37.96, 38.3 , 38.64, 38.98,
+       39.32, 39.66])
+>>> scatter(deg, temp, s=20, marker='o')""".format(n_ticks=_N_TICKS, high=_HIGH)
