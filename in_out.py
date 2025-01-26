a86de171376678c3a5a3ff579730e828f2f303dd
Dmitrii Beregovoi <dimaforth@gmail.com>
2025-01-26 16:58:14 +0000

    feat/090302-04: Add docstring to in_out

--- in_out.py
+++ in_out.py
@@ -2,23 +2,21 @@
 import os
 
 
-def save_plt(fig, filepath, dpi=None):
+def save_plt(fig, filepath, dpi='figure'):
     """
-    Save a figure to a file if file is not exist. If file exists - resave it
+    Save a Matplotlib figure to a file, overwriting the file if it already exists.
 
     Parameters
     ----------
-    fig: plt.figure
-        Matplotlib figure to save into a file
-    filepath: str or path-like or binary file-like
-        Path to save the figure
-    dpi: int or float or :class:`plt.figure` or None, default: :rc:`savefig.dpi`
-        The resolution in dots per inch. If 'plt.figure', use the figure's dpi value.
-        If None, ignore `dpi`.
+    fig : matplotlib.figure.Figure
+        The Matplotlib figure object to save.
+    filepath : str or path-like or binary file-like
+        The path or file-like object where the figure should be saved. If the file
+        already exists, it will be overwritten.
+    dpi: int, float or 'figure', default: rcParams["savefig.dpi"] (default: 'figure')
+        The resolution in dots per inch. If 'figure', use the figure's dpi value.
     """
     if os.path.isfile(filepath):
         os.remove(filepath)
-    if dpi is not None:
-        fig.savefig(filepath, dpi=dpi)
-    else:
-        fig.savefig(filepath)
+
+    fig.savefig(filepath, dpi=dpi)
